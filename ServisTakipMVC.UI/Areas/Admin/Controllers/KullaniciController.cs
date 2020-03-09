be6206c3362b9f98baa0defc4e77e87ff22b2d1d
R.Sercan Arıcı <sercanarici@hotmail.com>
2020-03-09 09:20:13 +0300

    kullanıcı ekleme silme eklendi

--- ServisTakipMVC.UI/Areas/Admin/Controllers/KullaniciController.cs
+++ ServisTakipMVC.UI/Areas/Admin/Controllers/KullaniciController.cs
@@ -13,9 +13,112 @@ namespace ServisTakipMVC.UI.Areas.Admin.Controllers
     public class KullaniciController : Controller
     {
         // GET: Admin/Kullanici
+        [LoginFilter]
         public ActionResult Index()
         {
+            using (KullaniciRepository repo = new KullaniciRepository())
+            {
+                var model = repo.Listele();
+                return View(model);
+            }
+        }
+
+        // GET: Admin/Kullanici/Create
+        [LoginFilter]
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        // POST: Admin/Kullanici/Create
+        [LoginFilter]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Kullanici model)
+        {
+            try
+            {
+
+                using (KullaniciRepository repo = new KullaniciRepository())
+                {
+                    model.KayitTarihi = DateTime.Now;
+                    repo.Ekle(model);
+                }
+
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(model);
+            }
+        }
+
+        // GET: Admin/Kullanici/Edit/5
+        [LoginFilter]
+        public ActionResult Edit(int id)
+        {
+            using (var repo = new KullaniciRepository())
+            {
+                var model = repo.Getir(x => x.Id == id);
+                return View(model);
+            }
+        }
+
+        // POST: Admin/Kullanici/Edit/5
+        [LoginFilter]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Kullanici model)
+        {
+            try
+            {
+                using (var repo = new KullaniciRepository())
+                {
+                    model.GuncellemeTarihi = DateTime.Now;
+                    repo.Guncelle(model);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {   
+                return View(model);
+            }
+        }
+
+        // GET: Admin/Kullanici/Delete/5
+        [LoginFilter]
+        public ActionResult Delete(int id)
+        {
+            using (var repo = new KullaniciRepository())
+            {
+                var model = repo.Getir(x => x.Id == id);
+                return View(model);
+            }
+        }
+
+        // POST: Admin/Kullanici/Delete/5
+        [LoginFilter]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, FormCollection fc)
+        {
+            try
+            {
+                using (var repo = new KullaniciRepository())
+                {
+                    var model = repo.Getir(x => x.Id == id);
+                    model.Silindi = true;
+                    repo.Guncelle(model);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
 
         public ActionResult Login()
@@ -38,6 +141,9 @@ namespace ServisTakipMVC.UI.Areas.Admin.Controllers
                         //TempData["Mesaj"] = new TempDataDictionary { { "class", "alert alert-success" }, { "mesaj", "Başarılı." } };
                         //TempData["Mesaj"] = new TempDataDictionary { { "class", "alert alert-success" }, { "mesaj", "Başarılı." } };
                         Session["User"] = durum;
+
+                        durum.SonGirisZamani = DateTime.Now;
+                        repo.Guncelle(durum);
                         return RedirectToAction("Index", "Home");
                     }
                     else
